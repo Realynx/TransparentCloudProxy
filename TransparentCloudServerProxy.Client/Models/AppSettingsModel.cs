d4c07fa3eb5a503d1ddda8150afe71d7a28be3c2
PoofImaFox <16710818+PoofImaFox@users.noreply.github.com>
2025-08-17 08:00:34 -0400

    feat: working settings view.

--- TransparentCloudServerProxy.Client/Models/AppSettingsModel.cs
+++ TransparentCloudServerProxy.Client/Models/AppSettingsModel.cs
@@ -1,28 +1,26 @@
 ﻿using System.ComponentModel;
 
+using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
 namespace TransparentCloudServerProxy.Client.Models {
-    public class AppSettingsModel {
+    public class AppSettingsModel : ReactiveObject {
         public AppSettingsModel() {
 
         }
 
-        [Reactive]
-        [property: Category("App Settings"), DisplayName("Theme")]
+        [Reactive, Category("App Settings"), DisplayName("Theme")]
         public string Theme { get; set; } = "Red";
 
-        [Reactive]
-        [property: Category("App Settings"), DisplayName("Save Logins")]
+
+        [Reactive, Category("App Settings"), DisplayName("Save Logins")]
         public bool SaveLogins { get; set; } = true;
 
-        [Reactive]
-        [property: Category("App Settings"), DisplayName("Saved Login Location")]
+        [Reactive, Category("App Settings"), DisplayName("Saved Login Location")]
         public string LoginCacheFile { get; set; } = "./config.bin";
 
 
-        [Reactive]
-        [property: Category("Proxy"), DisplayName("Default Packet Engine")]
+        [Reactive, Category("Proxy"), DisplayName("Default Packet Engine")]
         public string DefaultEngine { get; set; } = "NativeC";
 
     }
