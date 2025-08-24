a6e4e0a0170e1c2de4716211750ecf0af3fc49aa
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-08-24 01:03:00 -0400

    Set root namespace to Realynx.CatTail

--- CatTail/Program.cs
+++ CatTail/Program.cs
@@ -1,7 +1,7 @@
-﻿namespace CatTail {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-        }
+﻿namespace Realynx.CatTail;
+
+internal static class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
     }
 }
