25ef8ac401c74ddb4ef00cdd88a4870874e9977e
PoofImaFox <16710818+PoofImaFox@users.noreply.github.com>
2025-08-25 01:09:00 -0400

    feat: add install scripts

---
+++ TransparentCloudServerProxy.Client/Services/Deploy/OpenSshDeployService.cs
@@ -0,0 +1,19 @@
+﻿using Renci.SshNet;
+
+using System;
+
+namespace TransparentCloudServerProxy.Client.Services.Deploy {
+    public class OpenSshDeployService {
+        public OpenSshDeployService() {
+
+        }
+
+        public void DeployServer() {
+            using var client = new SshClient("sftp.foo.com", "guest", new PrivateKeyFile("path/to/my/key"));
+            client.Connect();
+
+            using var cmd = client.RunCommand("echo 'Hello World!'");
+            Console.WriteLine(cmd.Result);
+        }
+    }
+}
